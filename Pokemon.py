6448f79e2aa5748bd2e6bec9adcae2ae1f29e1ed
Aldo796 <mo.alden25@gmail.com>
2021-12-14 10:27:21 -0500

    Added all necessary get functions from personaltable

--- Pokemon.py
+++ Pokemon.py
@@ -7,10 +7,26 @@ from os import path
 import sys
 import UnityPy
 
-inputPath = "Original/personal_masterdatas"
-outputPath = "Edited/personal_masterdatas"
-pokemonAbilitiesPath = "Resources/Abilities.txt"
-pokemonTypePath = "Resources/Types.txt"
+originalPath = "Original/personal_masterdatas"
+editedPath = "Edited/personal_masterdatas"
+changelogPath = "Changelogs/personal_masterdatas.txt"
+pokemonAbilitiesPath = "Resources/abilities.txt"
+pokemonTypePath = "Resources/types.txt"
+pokemonNamePath = "Resources/pokemon.txt"
+
+PersonalTablePathID = "6925071152922426992"
+
+pathIDList = [PersonalTablePathID]
+
+compareList = ["Personal"]
+
+ability1 = "tokusei1"
+ability2 = "tokusei2"
+hiddenAbility = "tokusei3"
+type1 = "type1"
+type2 = "type2"
+
+iterateList = [ability1, ability2, hiddenAbility, type1, type2]
 
 def splitFile(src):
     if path.isfile(src):
@@ -21,4 +37,51 @@ def splitFile(src):
         input("Error "f"{src} does not exist")
         sys.exit()
         
-  
+abilityList = splitFile(pokemonAbilitiesPath)
+typeList = splitFile(pokemonTypePath)     
+       
+def formatAbility(abilityNum, abilityFlag): ##Change variable name, ability is to see if it's hidden or not
+    ability = abilityList[abilityNum]
+    abilityType = ""
+    if abilityFlag == 3:
+        abilityType = "Hidden: "
+    else:
+        abilityType = "Normal: "
+    formattedAbility = abilityType + ability
+    return formattedAbility
+
+def formatType(primaryType, secondaryType):
+    if primaryType == secondaryType: ##Monotype Pokemon
+        type = typeList[primaryType]
+        return type
+    else: ##Dual Type
+        type = typeList[primaryType] + " / " + typeList[secondaryType]
+        return type
+    
+def exists(src):
+    return path.exists(src)
+
+def getUnityTrees(unityfile, pathIDs = pathIDList):
+    
+    treeList = []
+
+    for obj in unityfile.objects:
+        if obj.path_id in pathIDs:
+            treeList.append(obj.read_typetree())
+    
+    return treeList
+    
+
+def formatString(pokemon, type, ability):
+
+if __name__ == '__main__':
+    
+    changelogWrite = open(changelogPath, "wt", encoding="utf8")
+    
+    original = UnityPy.load(originalPath)
+    edited = UnityPy.load(editedPath)
+    
+    originalTrees = getUnityTrees(original)
+    editedTrees = getUnityTrees(edited)
+    
+    
